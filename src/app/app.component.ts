a625bf6f0bacba8c0065cdeff2fd2363e3445258
matsumura-taichi <hiroto.in.the.cromagnons@gmail.com>
2017-11-18 21:33:52 +0900

    iframe要素に外部リソースをバインド

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,16 @@
 import { Component } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'my-app',
-  template: `<div [innerHTML]="safeMsg"></div>`
+  template: `<iframe [src]="safeUrl"></iframe>`
 })
 export class AppComponent  {
-  safeMsg: SafeHtml;
-  msg: string = `<script>window.alert("ようこそ！");</script>
-    <div style="font-size:20px;">
-      <p>WINGSプロジェクト</p>
-    </div>
-    <a href="http://www.wings.msn.to/">Web</a>
-    <button>同意する</button>
-    <input type="button" onClick="alert('OK')" value="クリック" />`;
+  safeUrl: SafeResourceUrl;
+  url = 'http://www.wings.msn.to/';
 
   constructor(private sanitizer: DomSanitizer) {
-    //msgプロパティの内容に信頼済みマークを付与
-    this.safeMsg = sanitizer.bypassSecurityTrustHtml(this.msg);
+    //リソースが信頼済みであることをマーク
+    this.safeUrl = sanitizer.bypassSecurityTrustResourceUrl(this.url);
   }
 }
