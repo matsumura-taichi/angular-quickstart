2732d22a47ea70642316086625ec7f86c535e405
matsumura-taichi <hiroto.in.the.cromagnons@gmail.com>
2017-11-18 20:45:48 +0900

    memberプロパティをコメントアウトして、「?.」演算子を追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<div>{{member.name}}</div>`,
+  template: `<div>{{member?.name}}</div>`,
 })
 export class AppComponent  {
-  member = {
-    name: '山田太郎',
-    age: 30
-  };
+//  member = {
+//    name: '山田太郎',
+//    age: 30
+//  };
 }
